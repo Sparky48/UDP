692804400df18a95f3867c43b8eeb6d992b228dd
Sparky <40040537+Sparky48@users.noreply.github.com>
2024-11-15 11:27:52 -0600

    Update RonsNJSTest.js

--- RonsNJSTest.js
+++ RonsNJSTest.js
@@ -7,3 +7,19 @@ server.on('error', (err) => {
    console.log(`server error:\n${err.stack}`);
    //server.close();
 });
+
+server.on('message', (msg, rinfo) => {
+   //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});   
+
+server.on('listening', () => {
+   const address = server.address();
+   //console.log(`server listening ${address.address}:${address.port}`);
+});   
+
+server.bind({
+   port: 2390,
+   address: '192.168.50.62'
+});
+
+//server.send('TurnCCW', 0, 7, 2390, 192.168.50.49);
